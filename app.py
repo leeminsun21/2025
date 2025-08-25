308b070381b3de8ec6fdfbc188e83904838691c3
leeminsun21 <24-10122@gsuite.gen.go.kr>
2025-08-25 21:16:30 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 def recommend_song(mood):
-    # mood_songs에 youtube_link 키 추가했어!
-    # dlals가 원하는 유튜브 링크로 바꿔주면 돼! (플레이리스트도 괜찮!)
+    # dlals가 추천한 신나는 곡들로 '신남' 분위기 완전 리뉴얼!
     mood_songs = {
         '행복함': [
             {"title": "Happy", "artist": "Pharrell Williams", "youtube_link": "https://www.youtube.com/watch?v=y6Sxv-sUYtM"},
@@ -16,9 +15,10 @@ def recommend_song(mood):
             {"title": "밤편지", "artist": "아이유", "youtube_link": "https://www.youtube.com/watch?v=F0z-9yq3qL8"},
             {"title": "늦은 밤 너의 집 앞 골목길에서", "artist": "노을", "youtube_link": "https://www.youtube.com/watch?v=0pL92v1m7D8"}
         ],
-        '신남': [
-            {"title": "Dynamite", "artist": "BTS", "youtube_link": "https://www.youtube.com/watch?v=gdZLi9oWNZg"},
-            {"title": "DDU-DU DDU-DU", "artist": "BLACKPINK", "youtube_link": "https://www.youtube.com/watch?v=IHNzOHi8sJs"}
+        '신남': [ # <<< 여기 dlals가 알려준 곡들로 변경됨!
+            {"title": "진격의 방탄소년단", "artist": "방탄소년단", "youtube_link": "https://youtu.be/7RsFNXsvusw?si=6ouFa2WMrn7CKdBS"},
+            {"title": "페더웨이", "artist": "보이넥스트도어", "youtube_link": "https://youtu.be/I6StVU3kABM?si=cPHdDdNcVKb1PxR_"},
+            {"title": "아주 나이스", "artist": "세븐틴", "youtube_link": "https://youtu.be/pOA-XQsEG44?si=ZoIR7LP1Rsr2pQ2G"}
         ],
         '에너지 충전': [
             {"title": "Don't Stop Me Now", "artist": "Queen", "youtube_link": "https://www.youtube.com/watch?v=HgzGwKwLmgM"},
@@ -28,15 +28,15 @@ def recommend_song(mood):
     return mood_songs.get(mood, [])
 
 
-# 페이지 설정 (요건 똑같아!)
+# 페이지 설정
 st.set_page_config(
-    page_title="분위기별 노래 추천 웹 앱🎶",
+    page_title="dlals의 분위기별 노래 추천 웹 앱🎶",
     page_icon="🎶"
 )
 
 # 웹 앱 메인 제목
-st.title("🎶 분위기별 노래 추천 웹 앱 🎶")
-st.write("오늘 어떤 분위기인지 알려주면 부힛이 찰떡같은 노래 추천해줄게!")
+st.title("🎶 dlals의 분위기별 노래 추천 웹 앱 🎶")
+st.write("dlals, 오늘 어떤 분위기인지 알려주면 부힛이 찰떡같은 노래 추천해줄게!")
 
 # 분위기 선택 옵션
 mood_options = ['선택해주세요', '행복함', '슬픔', '차분함', '신남', '에너지 충전']
@@ -50,7 +50,6 @@ if selected_mood != '선택해주세요':
     if recommended_songs:
         for song in recommended_songs:
             # HTML 마크다운으로 유튜브 링크를 클릭 가능한 형태로 보여주기!
-            # target='_blank' 이걸 넣어주면 새 탭으로 열려!
             st.markdown(f"- <a href='{song['youtube_link']}' target='_blank'><b>{song['title']}</b> by {song['artist']} (🎵 유튜브에서 듣기)</a>", unsafe_allow_html=True)
     else:
         st.write("아직 이 분위기에는 추천 곡이 없네 ㅠㅠ 다른 분위기를 선택해볼까?")
